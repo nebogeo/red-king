56ab12beed855011361a5a19132f352b1b61cac8
dave griffiths <dave@fo.am>
2016-05-12 17:59:11 +0100

    wsgi thing

--- redkingweb/redkingweb/wsgi.py
+++ redkingweb/redkingweb/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redkingweb.settings")
-
+sys.path.append('/var/www/redking/')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

caa0c61560064be26fb386b2372bbb24a3258330
dave griffiths <dave@fo.am>
2016-06-03 23:43:37 +0100

    added techno script

---
+++ redkingweb/sim/techno.py
@@ -0,0 +1,20 @@
+import math
+
+class techno:
+    def __init__(self,cutoff,resonance):
+        self.s = [0,0,0]
+        self.f = cutoff
+        self.q = resonance
+
+    # resonant low pass filter
+    def lpf_modf(self, src):
+        fb=self.q+self.q*(1-self.f)
+        self.s[0]+=self.f*(src-self.s[0])+fb*(self.s[0]-self.s[1])
+        self.s[1]+=self.f*(self.s[0]-self.s[1])
+        return self.s[1]
+
+    def saw(self,i):
+        return math.fmod(i/(2*math.pi),1)*2-1
+
+    def generate(self,i):
+        return self.lpf_modf(self.saw(i))

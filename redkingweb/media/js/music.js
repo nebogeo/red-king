7fc5eab7a508d631a107351bbf707450e9308a37
dave griffiths <dave@fo.am>
2016-11-09 14:52:06 +0000

    big sonification update, some style

--- redkingweb/media/js/music.js
+++ redkingweb/media/js/music.js
@@ -1,3 +1,19 @@
+// Red King Sonification Copyright (C) 2016-2017 FoAM Kernow
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////
+
 var context;
 var bufferLoader;
 
@@ -8,24 +24,27 @@ function sound_handler() {
     this.running = false;
 
     this.buffer_load_completed = function(buffer,h) { 
-	console.log("buffer load complete");
 	h.running=true;
 	h.reset_timing();
     }    
 
-    this.loop_length = 0.5;
+    this.loop_length = 0.75;
     this.safe_time = 0.2;
     this.logical_time = 0
     this.next_update_time = 0
     this.sequence = 0;
 
+    this.sample_list = []
+    for (var i=1; i<7; i++) {
+	this.sample_list.push("media/sound/dx7-o-"+i+".wav");
+    }
+    for (var i=1; i<7; i++) {
+	this.sample_list.push("media/sound/dx7-"+i+".wav");
+    }
+    
     this.buffer_loader = new BufferLoader(
         this.context,
-        [
-	    "media/sound/BT3AADA.WAV",
-	    "media/sound/HANDCLP2.WAV",
-	    "media/sound/ST0TAS3.WAV"
-        ],
+        this.sample_list,
 	this.buffer_load_completed,this
     );
 
@@ -43,7 +62,23 @@ function sound_handler() {
 	this.logical_time = this.context.currentTime+this.loop_length;
 	this.next_update_time = this.logical_time-this.safe_time;
     }
+    
+    this.make_sound = function(type,i,strains,length,tempo) {
+	
+	var x = Math.floor((strains[i][0]/length)*10);
+	var y = strains[i][1];
+	if (y>1) y=1;
+	if (y<0) y=0;
+	y=Math.floor(y*5);
+	y+=type*6;
+	var gain = 0.5;
+	if (type==1) gain=0.1;
 
+	this.play_sound(this.buffer_loader.bufferList[y],
+			this.logical_time+tempo*i+tempo/strains.length,
+			this.note_to_pitch(x+60),gain);
+    }
+    
     this.update = function(host,parasite) {
 	if (!this.running) return;
 	var t = this.context.currentTime;
@@ -51,31 +86,40 @@ function sound_handler() {
 
 	if (this.sequence==0) {
 	    host_strains = find_strain_centres(host);
-	    var host_tempo = this.loop_length/host_strains.length;
-	    
-	    for (var i=0; i<host_strains.length; i++) {
-		this.play_sound(this.buffer_loader.bufferList[0],
-				this.logical_time+host_tempo*i);
+	    var tempo = this.loop_length/host_strains.length;
+	    for (var i=0; i<host_strains.length; i++) {		
+		this.make_sound(this.sequence,i,host_strains,host.length,tempo);
 	    }
 	} else {
 	    parasite_strains = find_strain_centres(parasite);
-	    var host_tempo = this.loop_length/parasite_strains.length;
-	    
+	    var tempo = this.loop_length/parasite_strains.length;	    
 	    for (var i=0; i<parasite_strains.length; i++) {
-		this.play_sound(this.buffer_loader.bufferList[1],
-				this.logical_time+host_tempo*i);
+		var x = Math.floor((parasite_strains[i][0]/parasite.length)*10);
+		var y = parasite_strains[i][1];
+		if (y>1) y=1;
+		if (y<0) y=0;
+		y=Math.floor(y*9);
+		this.make_sound(this.sequence,i,parasite_strains,parasite.length,tempo);
 	    }
 	}
 			
 	this.sequence = (this.sequence+1)%2;
-	this.next_update_time+=this.loop_length;
-	this.logical_time+=this.loop_length;
+	this.next_update_time+=this.loop_length/2;
+	this.logical_time+=this.loop_length/2;
     }
 
-    this.play_sound = function(buffer, time) {
+    this.note_to_pitch = function(note) {
+	return (Math.pow(2,(note-69)/12));
+    }
+
+    this.play_sound = function(buffer, time, rate, vol) {
 	var source = this.context.createBufferSource();
 	source.buffer = buffer;
-	source.connect(this.context.destination);
+	source.playbackRate.value = rate;
+	var gain = this.context.createGain();
+	gain.connect(this.context.destination);
+	gain.gain.value = vol;
+	source.connect(gain);
 	source.start(time);
     }
 }
--- redkingweb/media/js/strain.js
+++ redkingweb/media/js/strain.js
@@ -1,44 +1,41 @@
-// code to find strains in a simulation time slice
-function find_strain_centres(level) {
-    var av = 0
-    var mx = 0
-    // find average and max for this slice
-    for (var i=0; i<level.length; i++) {
-        av+=level[i]
-        if (level[i]>mx) mx=level[i]
-    }
-    av/=level.length
-    av/=10 // threshold the detection
-    if (mx==0) mx=1
-    // record the positions of any strains above the threshold
-    // until they drop below the threshold
-    var in_strain = false
-    var strain_start = 0
-    var strains = []
-    for (var i=0; i<level.length; i++) {
-        if (!in_strain && level[i]>av) {
-            in_strain = true
-            strain_start = i
-	}
-        if (in_strain && level[i]<av) {
-            in_strain = false
-            strains.push([strain_start,i])
-	}
-    }
-    // find the centres of the strains and record the maximum there
-    var strain_centres = []
-    for (var s=0; s<strains.length; s++) {
-	strain=strains[s]; 
-        var strain_max = 0
-        var strain_pos = 0
-        for (var i=strain[0]; i<strain[1]; i++) {
-            if (level[i]>strain_max) {
-                strain_max=level[i]
-                strain_pos=i
-	    }
-	}
-        strain_centres.push([strain_pos,strain_max])
-    }
+// Red King Sonification Copyright (C) 2016-2017 FoAM Kernow
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////
 
-    return strain_centres
+// find local maxima by comparing first derivatives,
+// when they switch from up to down, record the position
+function local_max(arr) {
+    var ret = [];
+    var last_deriv = 0;
+    for (var i=1; i<arr.length; i++) {
+        deriv = arr[i]-arr[i-1];
+        if (deriv<0 && last_deriv>0) {
+            ret.push(i)
+	}
+        last_deriv = deriv
+    }
+    return ret;
 }
+
+// return the position and strength of each strain
+function find_strain_centres(level) {
+    var strains = local_max(level);
+    var strain_centres = [];
+    for (var s=0; s<strains.length; s++) {
+        strain_centres.push([strains[s],level[strains[s]]])
+    }
+    return strain_centres;
+}
+
